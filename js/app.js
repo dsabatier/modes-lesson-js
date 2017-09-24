e941e3083deae07541197f2ea2378518a704b605
Dominic <dsabatier@gmail.com>
2017-09-23 19:14:37 -0600

    adding style for button playing, fixing key hold bug

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
 // 65 87 83 69 68 70 84 71 89 72 85 74 75
 // A  W  S  E  D  F  T  G  Y  H  U  J  K
 // C3 C# D  Eb E  F  F# G  Ab A  Bb B  C4
-  window.addEventListener('keydown', function(event) {
-    const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
 
-    if(audio) {
-      audio.currentTime = 0;
-      audio.play();
-    }
-  });
+let lastKeyCode = 0;
+
+window.addEventListener('keydown', function(event) {
+  const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${event.keyCode}']`);
+
+  if(audio && this.lastKey != event.keyCode) {
+    this.lastKey = event.keyCode;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+  }
+});
+
+window.addEventListener('keyup', function(event) {
+  const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${event.keyCode}']`);
+
+  if(audio) {
+    this.lastKey = 0;
+    //audio.pause();
+    //audio.currentTime = 0;
+    key.classList.remove('playing');
+  }
+});
